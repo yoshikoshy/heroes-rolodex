6caa835632940b9c92b5676d8b11f5f38e2ffff6
Sean Koshy <sean.c.koshy@gmail.com>
2020-04-14 13:09:28 -0400

    Updates

---
+++ precache-manifest.e1894d039d9389ad795259a4ba0433c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "311ce4809c7ca427857e611059416e5b",
+    "url": "/heroes-rolodex/index.html"
+  },
+  {
+    "revision": "dc5a08b70b2bf5a039a2",
+    "url": "/heroes-rolodex/static/css/main.73e67af9.chunk.css"
+  },
+  {
+    "revision": "59a0e7775060b52d10fb",
+    "url": "/heroes-rolodex/static/js/2.1322b2b6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/heroes-rolodex/static/js/2.1322b2b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc5a08b70b2bf5a039a2",
+    "url": "/heroes-rolodex/static/js/main.41b6e4bc.chunk.js"
+  },
+  {
+    "revision": "2d27bd402c50841e6dc1",
+    "url": "/heroes-rolodex/static/js/runtime-main.7e0f0674.js"
+  }
+]);
